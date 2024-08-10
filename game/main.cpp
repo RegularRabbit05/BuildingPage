67349290d46bb12da9e4abfb6d53d610ab7ff69d
matteocapecci05 <69511985+RegularRabbit05@users.noreply.github.com>
2024-08-10 18:22:36 +0000

    Commit

--- game/main.cpp
+++ game/main.cpp
@@ -28,16 +28,31 @@ int main() {
     base.model = LoadModel("Graphics/base.glb");
     base.anim = LoadModelAnimations("Graphics/base.glb", &base.count);
 
+    AnimatedModel cube[2];
+    cube[0].model = LoadModel("Graphics/cube.glb");
+    cube[0].anim = LoadModelAnimations("Graphics/cube.glb", &cube[0].count);
+    cube[1].model = LoadModel("Graphics/cube.glb");
+    cube[1].anim = LoadModelAnimations("Graphics/cube.glb", &cube[1].count);
+    cube[1].frame = (cube[1].anim->frameCount / 2);
+
     while (!WindowShouldClose()) {
         base.frame++;
         if (base.frame >= base.anim->frameCount) base.frame = 0;
         UpdateModelAnimation(base.model, *base.anim, base.frame);
+        cube[0].frame++;
+        if (cube[0].frame >= cube[0].anim->frameCount) cube[0].frame = 0;
+        UpdateModelAnimation(cube[0].model, *cube[0].anim, cube[0].frame);
+        cube[1].frame++;
+        if (cube[1].frame >= cube[1].anim->frameCount) cube[1].frame = 0;
+        UpdateModelAnimation(cube[1].model, *cube[1].anim, cube[1].frame);
 
         BeginDrawing();
         ClearBackground(BLANK);
 
         BeginMode3D(camera);
         DrawModel(base.model, (Vector3){0, 0, 0}, 1.0f, WHITE);
+        DrawModel(cube[0].model, (Vector3){0, 0, 0}, 1.0f, WHITE);
+        DrawModel(cube[1].model, (Vector3){0, 0, 0}, 1.0f, WHITE);
         EndMode3D();
 
         EndDrawing();
@@ -45,6 +60,11 @@ int main() {
 
     UnloadModel(base.model);
     UnloadModelAnimations(base.anim, base.count);
+
+    for (int i = 0; i < 2; i++) {
+        UnloadModel(cube[i].model);
+        UnloadModelAnimations(cube[i].anim, cube[i].count);
+    }
 
     CloseWindow();
     return 0;
